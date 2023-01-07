dfbb266894e2ca855a66a071cade9c15335c1709
kurema <kurema_makoto_software@yahoo.co.jp>
2023-01-08 01:56:57 +0900

    Add simple library

---
+++ lib/dotnet/AozoraGaijiChuki/AozoraGaijiChuki/GaijiChuki.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+namespace Aozora.GaijiChuki;
+
+public static class Manager
+{
+    public static Xsd.dictionary? Instance { get; private set; }
+    public static Xsd.dictionary? LoadContent()
+    {
+        if (Instance is not null) return Instance;
+        using var stream = Assembly.GetAssembly(typeof(Manager))?.GetManifestResourceStream("Aozora.GaijiChuki.Chuki.gz");
+        if (stream is null) return null;
+        using var gz = new System.IO.Compression.GZipStream(stream, System.IO.Compression.CompressionMode.Decompress);
+        var xr = new System.Xml.Serialization.XmlSerializer(typeof(Xsd.dictionary));
+        try
+        {
+            return Instance = xr.Deserialize(gz) as Xsd.dictionary;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public static async Task<Xsd.dictionary?> LoadContentAsync() => await Task.Run(LoadContent).ConfigureAwait(false);
+}
---
+++ lib/dotnet/AozoraGaijiChuki/TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace TestProject1;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var content= Aozora.GaijiChuki.Manager.LoadContent();
+
+        Assert.NotNull(content);
+        Assert.Equal("一", content.kanji.page[0].radical.characters.character[0]);
+        Assert.Equal("いち", content.kanji.page[0].radical.readings.reading[0]);
+        Assert.Equal("𠂉", content.kanji.page[0].entries[0].characters.character[0]);
+        Assert.Equal("「尓－小」、第4水準2-1-1", content.kanji.page[0].entries[0].note.full);
+        Assert.Equal("「尓－小」", content.kanji.page[0].entries[0].note.description);
+    }
+}
