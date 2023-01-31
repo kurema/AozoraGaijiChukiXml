b5dc2d9d7254ea44092552f268206acb7b584278
kurema <kurema_makoto_software@yahoo.co.jp>
2023-01-31 23:16:53 +0900

    Update Manager.cs

--- lib/dotnet/AozoraGaijiChuki/Manager.cs
+++ lib/dotnet/AozoraGaijiChuki/Manager.cs
@@ -1,5 +1,6 @@
 ﻿//using Aozora.GaijiChuki.Xsd;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Reflection;
 
@@ -7,77 +8,84 @@ namespace Aozora.GaijiChuki;
 
 public static class Manager
 {
-    //中身は普通に書き換え可能。書き換えないでください。
-    public static Xsd.dictionary? Instance { get; private set; }
+	//中身は普通に書き換え可能。書き換えないでください。
+	public static Xsd.dictionary? Instance { get; private set; }
 
-    public static Xsd.dictionary? GetContentOrLoad() => Instance ??= LoadContent();
-    public static async Task<Xsd.dictionary?> GetContentOrLoadAsync() => Instance ??= await LoadContentAsync().ConfigureAwait(false);
+	public static Xsd.dictionary? GetContentOrLoad() => Instance ??= LoadContent();
+	public static async Task<Xsd.dictionary?> GetContentOrLoadAsync() => Instance ??= await LoadContentAsync().ConfigureAwait(false);
 
-    public static Xsd.dictionary? LoadContent()
-    {
-        //if (Instance is not null) return Instance;
-        using var stream = Assembly.GetAssembly(typeof(Manager))?.GetManifestResourceStream("Aozora.GaijiChuki.Chuki.gz");
-        if (stream is null) return null;
-        using var gz = new System.IO.Compression.GZipStream(stream, System.IO.Compression.CompressionMode.Decompress);
-        var xr = new System.Xml.Serialization.XmlSerializer(typeof(Xsd.dictionary));
-        try
-        {
-            return Instance = xr.Deserialize(gz) as Xsd.dictionary;
-        }
-        catch
-        {
-            return null;
-        }
-    }
+	public static Xsd.dictionary? LoadContent()
+	{
+		//if (Instance is not null) return Instance;
+		using var stream = Assembly.GetAssembly(typeof(Manager))?.GetManifestResourceStream("Aozora.GaijiChuki.Chuki.gz");
+		if (stream is null) return null;
+		using var gz = new System.IO.Compression.GZipStream(stream, System.IO.Compression.CompressionMode.Decompress);
+		var xr = new System.Xml.Serialization.XmlSerializer(typeof(Xsd.dictionary));
+		try
+		{
+			return Instance = xr.Deserialize(gz) as Xsd.dictionary;
+		}
+		catch
+		{
+			return null;
+		}
+	}
 
-    public static async Task<Xsd.dictionary?> LoadContentAsync() => await Task.Run(LoadContent).ConfigureAwait(false);
+	public static async Task<Xsd.dictionary?> LoadContentAsync() => await Task.Run(LoadContent).ConfigureAwait(false);
 
-    public static class Toc
-    {
-        //ところでfieldキーワードはC# 12に延長。
-        //https://github.com/dotnet/csharplang/issues/140#issuecomment-1209645505
-        static ReadOnlyDictionary<string, ReadOnlyMemory<Xsd.page>>? _RadicalCharacters;
-        public static ReadOnlyDictionary<string, ReadOnlyMemory<Xsd.page>>? RadicalCharacters
-            => _RadicalCharacters ??= Instance?.kanji.page.SelectMany(a => a.radical.characters.character.Select(b => (b, a))).GroupBy(a => a.b).ToDictionary(a => a.Key, a => new ReadOnlyMemory<Xsd.page>(a.Select(c => c.a).ToArray())).AsReadOnly();
+	public static class Toc
+	{
+		//ところでfieldキーワードはC# 12に延長。
+		//https://github.com/dotnet/csharplang/issues/140#issuecomment-1209645505
+		static ReadOnlyDictionary<string, ReadOnlyMemory<Xsd.page>>? _RadicalCharacters;
+		public static ReadOnlyDictionary<string, ReadOnlyMemory<Xsd.page>>? RadicalCharacters
+		{
+			get
+			{
+				var result = Instance?.kanji.page.SelectMany(a => a.radical.characters.character.Select(b => (b, a))).GroupBy(a => a.b).ToDictionary(a => a.Key, a => new ReadOnlyMemory<Xsd.page>(a.Select(c => c.a).ToArray()));
+				if (result is null) return null;
+				return new ReadOnlyDictionary<string, ReadOnlyMemory<Xsd.page>>(new SortedList<string, ReadOnlyMemory<Xsd.page>>(result));
+			}
+		}
 
-        private static Xsd.page[][]? _RadicalFromStroke;
+		private static Xsd.page[][]? _RadicalFromStroke;
 
-        public static Xsd.page[][]? RadicalFromStroke
-        {
-            get
-            {
-                if (_RadicalFromStroke is not null) return _RadicalFromStroke;
-                if (Instance is null) return null;
-                var result = new List<Xsd.page>[Instance.toc.strokesToRadical.strokes.Length + 1];
-                for (int i = 0; i < result.Length; i++) result[i] = new List<Xsd.page>();
-                foreach (var item in Instance.kanji.page)
-                {
-                    foreach (var item3 in Instance.toc.strokesToRadical.strokes)
-                    {
-                        if (item.radical.characters.character.Any(item3.Value.Contains))
-                        {
-                            result[item3.stroke].Add(item);
-                        }
-                    }
-                }
-                return _RadicalFromStroke = result.Select(a => a.ToArray()).ToArray();
-            }
-        }
+		public static Xsd.page[][]? RadicalFromStroke
+		{
+			get
+			{
+				if (_RadicalFromStroke is not null) return _RadicalFromStroke;
+				if (Instance is null) return null;
+				var result = new List<Xsd.page>[Instance.toc.strokesToRadical.strokes.Length + 1];
+				for (int i = 0; i < result.Length; i++) result[i] = new List<Xsd.page>();
+				foreach (var item in Instance.kanji.page)
+				{
+					foreach (var item3 in Instance.toc.strokesToRadical.strokes)
+					{
+						if (item.radical.characters.character.Any(item3.Value.Contains))
+						{
+							result[item3.stroke].Add(item);
+						}
+					}
+				}
+				return _RadicalFromStroke = result.Select(a => a.ToArray()).ToArray();
+			}
+		}
 
-        private static (string, Xsd.page)[]? _RadicalFromReadings;
+		private static (string, Xsd.page)[]? _RadicalFromReadings;
 
-        public static (string, Xsd.page)[]? RadicalFromReadings
-        {
-            get
-            {
-                return _RadicalFromReadings ??= gen();
+		public static (string, Xsd.page)[]? RadicalFromReadings
+		{
+			get
+			{
+				return _RadicalFromReadings ??= gen();
 
-                static (string, Xsd.page)[]? gen()
-                {
-                    if (Instance is null) return null;
-                    return Instance.kanji.page.SelectMany(page => page.radical.readings.reading.Select(reading => (reading, page))).OrderBy(a => a.reading).ToArray();
-                }
-            }
-        }
-    }
+				static (string, Xsd.page)[]? gen()
+				{
+					if (Instance is null) return null;
+					return Instance.kanji.page.SelectMany(page => page.radical.readings.reading.Select(reading => (reading, page))).OrderBy(a => a.reading).ToArray();
+				}
+			}
+		}
+	}
 }
